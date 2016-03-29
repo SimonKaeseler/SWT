6c030489756ff1a6a6c5292c6726f0957b9e75f9
SimonKaeseler <simonkaeseler@gmail.com>
2016-03-29 12:24:11 +0200

    MoreTEsts great commitmessage

--- VectorCalc/VectorCalc/VectorCalc/Implementation/VectorCalculator.cs
+++ VectorCalc/VectorCalc/VectorCalc/Implementation/VectorCalculator.cs
@@ -12,5 +12,31 @@ namespace VectorCalc.Implementation
             var result = Math.Pow(vec1.x, 2) + Math.Pow(vec1.y, 2) + Math.Pow(vec2.x, 2) + Math.Pow(vec2.y, 2);
             return result;
         }
+
+        public double SubTwoVectors(Cartesian2DVector vec1, Cartesian2DVector vec2)
+        {
+            return (Math.Pow(vec1.x, 2) + Math.Pow(vec1.y, 2)) - (Math.Pow(vec2.x, 2) + Math.Pow(vec2.y, 2));
+        }
+
+        public Cartesian2DVector Scale(Cartesian2DVector vec1, double i)
+        {
+            Cartesian2DVector vec2 = new Cartesian2DVector() {y = vec1.y*i,x= vec1.x*i};    
+            return vec2;
+        }
+
+        public double Dot(Cartesian2DVector vec1, Cartesian2DVector vec2)
+        {
+            return (vec1.x*vec2.x) + (vec1.y*vec2.y);
+        }
+
+        public double AngleBetween(Cartesian2DVector vec1, Cartesian2DVector vec2)
+        {
+            var dotproduct = Dot(vec1, vec2);
+            var length = Math.Sqrt((Math.Pow(vec1.x, 2) + Math.Pow(vec1.y, 2)))*
+                         Math.Sqrt((Math.Pow(vec2.x, 2) + Math.Pow(vec2.y, 2)));
+
+            var angle = dotproduct/length;
+            return Math.Acos(angle);
+        }
     }
 }
--- VectorCalc/VectorCalc/VectorCalc/Tests/VectorCalcUnitTest.cs
+++ VectorCalc/VectorCalc/VectorCalc/Tests/VectorCalcUnitTest.cs
@@ -35,5 +35,89 @@ namespace Testsuite
             Assert.That(_calculator.AddToVectors(vec1,vec2),Is.EqualTo(36));
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void SubTwoVectors()
+        {
+            var vec1 = new Cartesian2DVector()
+            {
+                y = 0,
+                x = 0
+            };
+
+            var vec2 = new Cartesian2DVector()
+            {
+                y = 0,
+                x = 0
+            };
+            Assert.That(_calculator.SubTwoVectors(vec1,vec2),Is.EqualTo(0));
         }
+
+
+        /// <summary>
+        /// 
+       // / </summary>
+        [Test]
+        public void ScaleVector()
+        {
+            double factor = 3;
+            var vec1 = new Cartesian2DVector()
+            {
+                y = 3,
+                x = 3
+            };
+
+            var vec2 = new Cartesian2DVector()
+            {
+                y = 9,
+                x = 9
+            };
+            Assert.That(_calculator.Scale(vec1,factor),Is.EqualTo(vec2));
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void DotProcuctOfTwoVectors_NoCondition_CorrectREsult()
+        {
+            var vec1 = new Cartesian2DVector()
+            {
+                y = 3,
+                x = 3
+            };
+            var vec2 = new Cartesian2DVector()
+            {
+                y = 3,
+                x = 3
+            };
+            
+            Assert.That(_calculator.Dot(vec1,vec2),Is.EqualTo(18));
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void AngleBetween_NoCondition_CorrectAngle()
+        {
+            var vec1 = new Cartesian2DVector()
+            {
+                y = 3,
+                x = 3
+            };
+            var vec2 = new Cartesian2DVector()
+            {
+                y = -3,
+                x = -3
+            };
+            Assert.That(_calculator.AngleBetween(vec1,vec2),Is.EqualTo(180.0));
+        }
+
+    }
     }
